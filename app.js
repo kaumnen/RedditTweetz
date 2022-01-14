347663b4483e1edc055f8c078216fe57b4d0357b
kaumnen <main@komnen.xyz>
2022-01-14 15:03:31 +0100

    rewrite logic in starting file

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-require("dotenv").config();
-const snoowrap = require("snoowrap");
+import { scrapeSubreddit } from './subreddits.js';
 
-const redditAPI = new snoowrap({
-    userAgent: 'reddit-bot-example-node',
-    clientId: process.env.REDDIT_CLIENT_ID,
-    clientSecret: process.env.REDDIT_CLIENT_SECRET,
-    username: process.env.REDDIT_USERNAME,
-    password: process.env.REDDIT_PASSWORD
-});
+const test = await scrapeSubreddit();
+
+console.log(test);
