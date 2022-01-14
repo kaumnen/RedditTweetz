edb6b34f9d7ed4862ce73c9dbb632978d23512f6
kaumnen <main@komnen.xyz>
2022-01-14 14:17:35 +0100

    initialize snoowrap object

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 require("dotenv").config();
 const snoowrap = require("snoowrap");
+
+const redditAPI = new snoowrap({
+    userAgent: 'reddit-bot-example-node',
+    clientId: process.env.REDDIT_CLIENT_ID,
+    clientSecret: process.env.REDDIT_CLIENT_SECRET,
+    username: process.env.REDDIT_USERNAME,
+    password: process.env.REDDIT_PASSWORD
+});
