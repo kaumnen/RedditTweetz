47dd851b7bd36df2f56ac991633ad15183ec2212
kaumnen <main@komnen.xyz>
2022-01-14 15:03:17 +0100

    scrape certain subreddit

---
+++ subreddits.js
@@ -0,0 +1,28 @@
+import {} from "dotenv/config";
+import snoowrap from "snoowrap";
+
+export async function scrapeSubreddit() {
+    const redditAPI = new snoowrap({
+        userAgent: 'reddit-bot-example-node',
+        clientId: process.env.REDDIT_CLIENT_ID,
+        clientSecret: process.env.REDDIT_CLIENT_SECRET,
+        username: process.env.REDDIT_USERNAME,
+        password: process.env.REDDIT_PASSWORD
+    });
+    
+    //get top posts from subreddit
+    const subreddit = await redditAPI.getSubreddit("askReddit");
+    const topPosts = await subreddit.getTop({time: "week", limit: 7});
+    
+    let postsData = [];
+    
+    topPosts.forEach((post) => {
+        postsData.push({
+          link: post.url,
+          text: post.title,
+          score: post.score
+        })
+      });
+    
+    return postsData;
+};
