0cddbb2490f882477f198dcbb606f5eac639bc4a
kaumnen <main@komnen.xyz>
2022-01-15 13:40:39 +0100

    rewrite entry point, just call function and set interval

--- app.js
+++ app.js
@@ -1,20 +1,4 @@
-import { makeTweet } from './askReddit.js';
-import { makeReplies } from './askRedditComments.js';
-import "dotenv/config";
-import { TwitterApi } from "twitter-api-v2";
+import { tweetIt } from './sendTweet.js';
 
-const client = new TwitterApi({
-    appKey: process.env.TWITTER_CONSUMER_KEY,
-    appSecret: process.env.TWITTER_CONSUMER_SECRET,
-    accessToken: process.env.TWITTER_ACCESS_KEY,
-    accessSecret: process.env.TWITTER_ACCESS_SECRET
-});
-
-const rwClient = client.readWrite;
-
-let askRedditHottest = await makeTweet();
-let askRedditComments = await makeReplies();
-
-askRedditComments.unshift(askRedditHottest);
-
-rwClient.v2.tweetThread(askRedditComments).then(console.log("let gooo!"));
+tweetIt();
+//setInterval(tweetIt, 1000 * 21600);
