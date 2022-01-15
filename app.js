01b3e3b156b791174918e40b9f9437bcfdbc4e22
kaumnen <main@komnen.xyz>
2022-01-15 12:53:57 +0100

    import funtions for post and comments, submit as a thread

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-import { getPosts } from './subreddits.js';
+import { makeTweet } from './askReddit.js';
+import { makeReplies } from './askRedditComments.js';
 import "dotenv/config";
 import { TwitterApi } from "twitter-api-v2";
 
@@ -11,18 +12,9 @@ const client = new TwitterApi({
 
 const rwClient = client.readWrite;
 
-const topPosts = await getPosts();
+let askRedditHottest = await makeTweet();
+let askRedditComments = await makeReplies();
 
-// rwClient.v2.tweetThread([
-//     "Hey there! I'm a bot that tweets the top posts from /r/askReddit every week. Check it out:",
-//     "https://www.reddit.com/r/askReddit/top/?t=week",
-//     "test"
-// ]);
+askRedditComments.unshift(askRedditHottest);
 
-// for(let i = 0; i < 1; i++) {
-//     rwClient.v2.tweet(`⚡${topPosts[i].text}⚡\n ${topPosts[i].score} upvotes so far! \n ${topPosts[i].link}`);
-// }
-
-// topPosts.forEach(post => {
-//     rwClient.v2.tweet(`⚡${post.text}⚡\n ${post.score} upvotes so far! \n ${post.link}`);
-// });
+rwClient.v2.tweetThread(askRedditComments).then(console.log("let gooo!"));
