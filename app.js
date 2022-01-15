f687bce7e3f99b1c7f96776ae73e154d766d744e
kaumnen <main@komnen.xyz>
2022-01-15 06:46:56 +0100

    add basic functionality, sending a tweet

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 import { scrapeSubreddit } from './subreddits.js';
+import "dotenv/config";
+import { TwitterApi } from "twitter-api-v2";
 
-const test = await scrapeSubreddit();
+const client = new TwitterApi({
+    appKey: process.env.TWITTER_CONSUMER_KEY,
+    appSecret: process.env.TWITTER_CONSUMER_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_KEY,
+    accessSecret: process.env.TWITTER_ACCESS_SECRET
+});
 
-console.log(test);
+const rwClient = client.readWrite;
+
+const topPosts = await scrapeSubreddit();
+
+rwClient.v2.tweet("Hello world!");
+
+// for(let i = 0; i < 1; i++) {
+//     rwClient.v2.tweet(`⚡${topPosts[i].text}⚡\n ${topPosts[i].score} upvotes so far! \n ${topPosts[i].link}`);
+// }
+
+// topPosts.forEach(post => {
+//     rwClient.v2.tweet(`⚡${post.text}⚡\n ${post.score} upvotes so far! \n ${post.link}`);
+// });
